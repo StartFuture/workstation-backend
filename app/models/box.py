3185bb37bcb9b711f8a46e6d13097bc5c62cff84
Mateus-Toni <mattytoni04@gmail.com>
2022-04-02 21:59:00 -0300

    Class create box
    Create new box on DB

--- app/models/box.py
+++ app/models/box.py
@@ -1,13 +1,46 @@
-
-
-
-class Box():
+from flask_restful import Resource, reqparse
+from defs_workstation import handle_data_box
+from . import dao as Bank
+        
+        
+class ShowListBox(Resource):
     
-    def __init__(self, id_adress, name, size, price_hour, description, activate):
-        self.id_adress = id_adress
-        self.name = name
-        self.size = size
-        self.price_hour = price_hour
-        self.description = description
-        self.activate = activate
+    def get(self):  
+        return handle_data_box()
+
+class CreateBox(Resource):
+    
+    def post(self):
+        argumentos = reqparse.RequestParser()
+        
+        argumentos.add_argument("cep_endereco")
+        argumentos.add_argument("nome")
+        argumentos.add_argument("preco_hora")
+        argumentos.add_argument("descricao")
+        argumentos.add_argument("largura")
+        argumentos.add_argument("altura")
+        argumentos.add_argument("comprimento")
+        
+        dados = argumentos.parse_args()
+        
+        id_address = Bank.DataBaseBox.get_id_address_by_cep(dados['cep_endereco'])
+        
+        try:
+            Bank.DataBaseBox.add_box(
+                id_address=id_address, 
+                name=dados['nome'], 
+                price_hour=dados['preco_hora'], 
+                description=dados['descricao'], 
+                width=dados['largura'], 
+                heigth=dados['altura'], 
+                depth=dados['comprimento']
+            )
+        except Exception as erros:
+            return {
+                "msg": "Erro de Banco de dados"
+            }
+        else:
+            return {
+                "msg": "sucessfull"
+            }
         
